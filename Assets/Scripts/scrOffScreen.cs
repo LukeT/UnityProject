0fd12c5869103b05919658444374d90cade776a4
Luke Thompson <luke.thompson@xeonr.com>
2015-04-22 14:16:35 +0100

    Upload Project

---
+++ Assets/Scripts/scrOffScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrOffScreen : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.tag == "Player") {
+			// Player has died;
+			return;
+		}
+	}
+
+}
---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+			if (Input.GetMouseButtonDown (0)) {
+				m_Jump = true;
+			}
+        }
+
+
+        private void FixedUpdate()
+        {
+			m_Character.Move (0.5f, false, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
