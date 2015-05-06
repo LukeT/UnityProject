c5f044d685524806f9640c43be8278ebfc3f7150
Luke Thompson <luke.thompson@xeonr.com>
2015-05-06 09:17:09 +0100

    Score on end screen

---
+++ Assets/scrDisplayScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrDisplayScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("called");
+		int Score = scrUpdateCounter.getScore ();
+
+		TextMesh textObject = GameObject.Find("score").GetComponent<TextMesh>();
+		textObject.text = "Score: "  + Score.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
